84b4b357192ccd406d426532282f2bb028b3f0fb
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2022-01-13 18:46:35 +0200

    task1

---
+++ js/task-01.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const categoriesEll = document.querySelector('ul#categories li.item');
+// const numberCategoriesEll = categoriesEll.length;
+// console.log(`Nunber of categories: ${numberCategoriesEll}`);
+
+// for (let i = 0; i < numberCategoriesEll; i += 1){
+//     const textContent = categoriesEll[i].firstElementChild.textContent;
+//     console.log(`Categories: ${textContent}`);
+
+//     const allElement = categoriesEll[i].lastElementchild;
+//     const numberOfelements = allElement.querySelectorAll('li');
+//     console.log(`Elements: ${numberOfelements.length}`)
+// };
