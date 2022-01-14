53ce30a754977ed2676a624c48ddd1382ac280a7
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2022-01-14 23:45:06 +0200

    taskOver-01

--- js/task-01.js
+++ js/task-01.js
@@ -12,15 +12,20 @@
 
 
 
-// const categoriesEll = document.querySelector('ul#categories li.item');
-// const numberCategoriesEll = categoriesEll.length;
-// console.log(`Nunber of categories: ${numberCategoriesEll}`);
 
-// for (let i = 0; i < numberCategoriesEll; i += 1){
-//     const textContent = categoriesEll[i].firstElementChild.textContent;
-//     console.log(`Categories: ${textContent}`);
 
-//     const allElement = categoriesEll[i].lastElementchild;
-//     const numberOfelements = allElement.querySelectorAll('li');
-//     console.log(`Elements: ${numberOfelements.length}`)
-// };
+const categoriesEll = document.querySelectorAll('ul#categories li.item');
+const numberCatItemEll = categoriesEll.length
+console.log(`Number of categories: ${numberCatItemEll}`);
+
+ 
+const allElementCategories = categoriesEll.forEach((titleCategories) => {
+    
+  const titleEll = titleCategories.firstElementChild.textContent;
+  console.log(`Categories: ${ titleEll }`);
+
+  
+  const numberOfEll = titleCategories.querySelectorAll('li');
+  console.log(`Elements: ${numberOfEll.length}`)
+
+});
